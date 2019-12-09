905074d16ac0202df308dff135f5ca515d764429
Mert Kaymaz <mertfener@gmail.com>
2019-12-09 23:32:24 +0300

    deneme
    deneme

---
+++ deneme1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Denemedir
+{
+    public class deneme1
+    {
+        public int id { get; set; }  
+        public int code { get; set; }
+        public string name { get; set; }
+    }
+}
